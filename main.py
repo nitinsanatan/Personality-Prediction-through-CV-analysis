04703625158b6c30995a7638e9041bafb45210ef
nitinsanatan <62790364+nitinsanatan@users.noreply.github.com>
2021-02-10 15:21:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,3 +31,93 @@ class train_model:
         
         self.mul_lr = linear_model.LogisticRegression(multi_class='multinomial', solver='newton-cg',max_iter =1000)
         self.mul_lr.fit(mainarray, train_y)
+        
+    def test(self, test_data):
+        try:
+            test_predict=list()
+            for i in test_data:
+                test_predict.append(int(i))
+            y_pred = self.mul_lr.predict([test_predict])
+            return y_pred
+        except:
+            print("All Factors For Finding Personality Not Entered!")
+            
+def check_type(data):
+    if type(data)==str or type(data)==str:
+        return str(data).title()
+    if type(data)==list or type(data)==tuple:
+        str_list=""
+        for i,item in enumerate(data):
+            str_list+=item+", "
+        return str_list
+    else:
+        return str(data)
+
+def prediction_result(top, aplcnt_name, cv_path, personality_values):
+    "after applying a job"
+    top.withdraw()
+    applicant_data={"Candidate Name":aplcnt_name.get(),  "CV Location":cv_path}
+    
+    age = personality_values[1]
+    
+    print("\n############# Candidate Entered Data #############\n")
+    print(applicant_data, personality_values)
+    
+    personality = model.test(personality_values)
+    print("\n############# Predicted Personality #############\n")
+    print(personality)
+    data = ResumeParser(cv_path).get_extracted_data()
+    
+    try:
+        del data['name']
+        if len(data['mobile_number'])<10:
+            del data['mobile_number']
+    except:
+        pass
+    
+    print("\n############# Resume Parsed Data #############\n")
+
+    for key in data.keys():
+        if data[key] is not None:
+            print('{} : {}'.format(key,data[key]))
+    
+    result=Tk()
+  #  result.geometry('700x550')
+    result.overrideredirect(False)
+    result.geometry("{0}x{1}+0+0".format(result.winfo_screenwidth(), result.winfo_screenheight()))
+    result.configure(background='White')
+    result.title("Predicted Personality")
+    
+    #Title
+    titleFont = font.Font(family='Arial', size=40, weight='bold')
+    Label(result, text="Result - Personality Prediction", foreground='green', bg='white', font=titleFont, pady=10, anchor=CENTER).pack(fill=BOTH)
+    
+    Label(result, text = str('{} : {}'.format("Name:", aplcnt_name.get())).title(), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
+    Label(result, text = str('{} : {}'.format("Age:", age)), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
+    for key in data.keys():
+        if data[key] is not None:
+            Label(result, text = str('{} : {}'.format(check_type(key.title()),check_type(data[key]))), foreground='black', bg='white', anchor='w', width=60).pack(fill=BOTH)
+    Label(result, text = str("perdicted personality: "+personality).title(), foreground='black', bg='white', anchor='w').pack(fill=BOTH)
+    
+    quitBtn = Button(result, text="Exit", command =lambda:  result.destroy()).pack()
+    
+    terms_mean = """
+# Openness:
+    People who like to learn new things and enjoy new experiences usually score high in openness. Openness includes traits like being insightful and imaginative and having a wide variety of interests.
+
+# Conscientiousness:
+    People that have a high degree of conscientiousness are reliable and prompt. Traits include being organised, methodic, and thorough.
+
+# Extraversion:
+    Extraversion traits include being; energetic, talkative, and assertive (sometime seen as outspoken by Introverts). Extraverts get their energy and drive from others, while introverts are self-driven get their drive from within themselves.
+
+# Agreeableness:
+    As it perhaps sounds, these individuals are warm, friendly, compassionate and cooperative and traits include being kind, affectionate, and sympathetic. In contrast, people with lower levels of agreeableness may be more distant.
+
+# Neuroticism:
+    Neuroticism or Emotional Stability relates to degree of negative emotions. People that score high on neuroticism often experience emotional instability and negative emotions. Characteristics typically include being moody and tense.    
+"""
+    
+    Label(result, text = terms_mean, foreground='green', bg='white', anchor='w', justify=LEFT).pack(fill=BOTH)
+
+    result.mainloop()            
