b7bbdcb64aec9b3ba7bbba1205e6fbc65258cde2
nitinsanatan <62790364+nitinsanatan@users.noreply.github.com>
2021-03-08 20:25:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -121,3 +121,98 @@ def prediction_result(top, aplcnt_name, cv_path, personality_values):
     Label(result, text = terms_mean, foreground='green', bg='white', anchor='w', justify=LEFT).pack(fill=BOTH)
 
     result.mainloop()            
+def perdict_person():
+    """Predict Personality"""
+    
+    # Closing The Previous Window
+    root.withdraw()
+    
+    # Creating new window
+    top = Toplevel()
+    top.geometry('700x500')
+    top.configure(background='black')
+    top.title("Apply For A Job")
+    
+    #Title
+    titleFont = font.Font(family='Helvetica', size=20, weight='bold')
+    lab=Label(top, text="Personality Prediction", foreground='red', bg='black', font=titleFont, pady=10).pack()
+
+    #Job_Form
+    job_list=('Select Job', '101-Developer at TTC', '102-Chef at Taj', '103-Professor at MIT')
+    job = StringVar(top)
+    job.set(job_list[0])
+
+    l1=Label(top, text="Applicant Name", foreground='white', bg='black').place(x=70, y=130)
+    l2=Label(top, text="Age", foreground='white', bg='black').place(x=70, y=160)
+    l3=Label(top, text="Gender", foreground='white', bg='black').place(x=70, y=190)
+    l4=Label(top, text="Upload Resume", foreground='white', bg='black').place(x=70, y=220)
+    l5=Label(top, text="Enjoy New Experience or thing(Openness)", foreground='white', bg='black').place(x=70, y=250)
+    l6=Label(top, text="How Offen You Feel Negativity(Neuroticism)", foreground='white', bg='black').place(x=70, y=280)
+    l7=Label(top, text="Wishing to do one's work well and thoroughly(Conscientiousness)", foreground='white', bg='black').place(x=70, y=310)
+    l8=Label(top, text="How much would you like work with your peers(Agreeableness)", foreground='white', bg='black').place(x=70, y=340)
+    l9=Label(top, text="How outgoing and social interaction you like(Extraversion)", foreground='white', bg='black').place(x=70, y=370)
+    
+    sName=Entry(top)
+    sName.place(x=450, y=130, width=160)
+    age=Entry(top)
+    age.place(x=450, y=160, width=160)
+    gender = IntVar()
+    R1 = Radiobutton(top, text="Male", variable=gender, value=1, padx=7)
+    R1.place(x=450, y=190)
+    R2 = Radiobutton(top, text="Female", variable=gender, value=0, padx=3)
+    R2.place(x=540, y=190)
+    cv=Button(top, text="Select File", command=lambda:  OpenFile(cv))
+    cv.place(x=450, y=220, width=160)
+    openness=Entry(top)
+    openness.insert(0,'1-10')
+    openness.place(x=450, y=250, width=160)
+    neuroticism=Entry(top)
+    neuroticism.insert(0,'1-10')
+    neuroticism.place(x=450, y=280, width=160)
+    conscientiousness=Entry(top)
+    conscientiousness.insert(0,'1-10')
+    conscientiousness.place(x=450, y=310, width=160)
+    agreeableness=Entry(top)
+    agreeableness.insert(0,'1-10')
+    agreeableness.place(x=450, y=340, width=160)
+    extraversion=Entry(top)
+    extraversion.insert(0,'1-10')
+    extraversion.place(x=450, y=370, width=160)
+
+    submitBtn=Button(top, padx=2, pady=0, text="Submit", bd=0, foreground='white', bg='red', font=(12))
+    submitBtn.config(command=lambda: prediction_result(top,sName,loc,(gender.get(),age.get(),openness.get(),neuroticism.get(),conscientiousness.get(),agreeableness.get(),extraversion.get())))
+    submitBtn.place(x=350, y=400, width=200)
+    
+
+    top.mainloop()
+
+def OpenFile(b4):
+    global loc;
+    name = filedialog.askopenfilename(initialdir="C:/Users/Batman/Documents/Programming/tkinter/",
+                            filetypes =(("Document","*.docx*"),("PDF","*.pdf*"),('All files', '*')),
+                           title = "Choose a file."
+                           )
+    try:
+        filename=os.path.basename(name)
+        loc=name
+    except:
+        filename=name
+        loc=name
+    b4.config(text=filename)
+    return
+
+
+
+if __name__ == "__main__":
+    model = train_model()
+    model.train()
+
+    root = Tk()
+    root.geometry('700x500')
+    root.configure(background='white')
+    root.title("Personality Prediction System")
+    titleFont = font.Font(family='Helvetica', size=25, weight='bold')
+    homeBtnFont = font.Font(size=12, weight='bold')
+    lab=Label(root, text="Personality Prediction System", bg='white', font=titleFont, pady=30).pack()
+    b2=Button(root, padx=4, pady=4, width=30, text="Predict Personality", bg='black', foreground='white', bd=1, font=homeBtnFont, command=perdict_person).place(relx=0.5, rely=0.5, anchor=CENTER)
+    root.mainloop()
